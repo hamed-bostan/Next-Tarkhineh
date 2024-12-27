4ca9948688c78bc1a4678933241816f84b19aae7
Hamed Bostan <Hamed.Bostan2@gmail.com>
2024-12-27 09:47:20 +0330

    fix(styles): adjust header-section on the landing page

--- src/components/landing/Header.js
+++ src/components/landing/Header.js
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <div className="relative">
+    <div className="relative mb-6">
       <img
         src="assets/images/bannerImages/slider_01.png"
         alt="banner image"
-        className="h-44 object-cover"
+        className="h-44 w-full object-cover"
       />
       <span className="absolute top-1/2 right-1/2 text-white -translate-y-1/2 translate-x-1/2 text-sm text-nowrap">
         تجربه غذای سالم و گیاهی به سبک ترخینه
