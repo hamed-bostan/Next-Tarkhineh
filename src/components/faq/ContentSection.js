3d8da09b8fffa9dd0e31a44a4ff8e7ec9427853a
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-05 13:09:51 +0330

    chore(faq): remove value item from faq details

--- src/components/faq/ContentSection.js
+++ src/components/faq/ContentSection.js
@@ -9,7 +9,7 @@ export default function ContentSection({ details }) {
   return (
     <Accordion type="single" collapsible className="w-full text-[#353535]">
       {details.map((item) => (
-        <AccordionItem value={item.value} className="px-4 py-2" key={item.id}>
+        <AccordionItem value={item.id} className="px-4 py-2" key={item.id}>
           <AccordionTrigger className="hover:no-underline p-0 text-start">
             {item.title}
           </AccordionTrigger>
