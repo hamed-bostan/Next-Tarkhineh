2f47cfbbaf53c084e3d7473ad791dfa421ef5d2f
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-06 23:50:32 +0330

    feat(landing page): add desktop version styles to menuSection

--- src/components/landing/MenuSection.js
+++ src/components/landing/MenuSection.js
@@ -23,15 +23,24 @@ const menuInformation = [
 
 export default function MenuSection() {
   return (
-    <div className="px-5 mb-16">
-      <h1 className="text-center mb-3 text-sm font-bold">منوی رستوران</h1>
-      <div className="grid grid-cols-2 place-items-center gap-y-16">
+    <div className="px-5 mb-16 text-[#353535]">
+      <h1 className="text-center mb-3 font-bold md:text-xl md:mb-7">
+        منوی رستوران
+      </h1>
+      <div className="grid grid-cols-2 place-items-center gap-y-16 md:grid-cols-4">
         {menuInformation.map((item) => {
           return (
-            <div className="relative h-28 w-28 flex justify-center" key={item.id}>
-              <img src={item.image} alt={item.text} className="h-full" />
-              <div className="bg-[#417F56] shadow-xl w-36 h-20 rounded-sm absolute top-1/2 -z-10"></div>
-              <span className="text-sm w-24 h-8 bg-white rounded-sm shadow-md flex items-center justify-center absolute -bottom-1/2 -translate-y-1/2">
+            <div
+              className="relative h-28 w-28 flex justify-center lg:w-36 lg:h-36"
+              key={item.id}
+            >
+              <img
+                src={item.image}
+                alt={item.text}
+                className="h-full w-full object-cover"
+              />
+              <div className="bg-[#417F56] shadow-xl w-36 h-20 rounded-sm absolute top-1/2 -z-10 lg:w-52 lg:h-24"></div>
+              <span className="text-xs w-24 h-8 bg-[#F9F9F9] rounded-sm shadow-md flex items-center justify-center absolute -bottom-1/2 -translate-y-1/2 md:text-base  lg:bottom-0 lg:translate-y-full lg:w-32 lg:h-10">
                 {item.text}
               </span>
             </div>
