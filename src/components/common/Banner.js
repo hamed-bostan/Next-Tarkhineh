9d5dd2a9c20fe0d200a171d8e8c20f275c0139e1
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-06 23:53:08 +0330

    fix(banner): adjust text style for desktop version

--- src/components/common/Banner.js
+++ src/components/common/Banner.js
@@ -10,7 +10,7 @@ export default function Banner({
   return (
     <div className={`relative h-44 mb-6 md:h-48 ${styleContainer}`}>
       <Image src={imageSrc} alt={text} width={300} height={150} className="h-full w-full object-cover" />
-      <span className="absolute top-1/2 right-1/2 text-[#E5F2E9] -translate-y-1/2 translate-x-1/2 text-nowrap md:text-2xl">
+      <span className="absolute top-1/2 right-1/2 text-[#E5F2E9] -translate-y-1/2 translate-x-1/2 text-nowrap md:text-xl">
         {text}
       </span>
       {isButton && (
