a619af0baf6f7b3cd044a57c29b648b60d7bdcb5
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-08 12:00:28 +0330

    fix(landing page): resolve desktop styles for BranchOverview

--- src/components/landing/branchOverview/BranchItem.js
+++ src/components/landing/branchOverview/BranchItem.js
@@ -12,13 +12,13 @@ export default function BranchItem({ branch }) {
 
 function DisplayingImage({ branch }) {
   return (
-    <div className="relative col-span-1 row-span-2 h-20 md:h-40 lg:h-52 md:row-span-1 lg:group-hover:h-44 transition-all duration-300">
+    <div className="relative col-span-1 row-span-2 h-20 md:h-40 lg:h-52 md:row-span-1 md:group-hover:h-36 lg:group-hover:h-44 transition-all duration-300">
       <Image
         width={100}
         height={100}
         src={branch.image}
         alt={branch.title}
-        className="w-full h-full object-cover group-hover:opacity-40"
+        className="w-full h-full object-cover"
       />
       <div className="hidden md:block absolute inset-0 bg-[#181818] opacity-0 group-hover:opacity-65 transition-opacity duration-300 z-20"></div>
       <Image
@@ -41,24 +41,22 @@ function DisplayingImage({ branch }) {
 
 function DisplayingDetails({ branch }) {
   return (
-    <div className="md:h-28">
-      <div className="p-2 col-span-1 row-span-2 text-center md:row-span-1 md:py-4">
-        <h3 className="text-sm font-medium text-[#353535] row-span-1 mb-1 md:mb-3 md:text-lg md:font-semibold">
-          {branch.title}
-        </h3>
-        <p className="text-xs text-[#717171] row-span-1 md:text-sm md:mb-4">
-          {branch.address}
-        </p>
-        <MyButton
-          containerStyle="hidden md:group-hover:flex transition-all duration-300"
-          label="صفحه شعبه"
-          variant="outline"
-          imageSrc="/assets/images/icons/arrow-left.svg"
-          alt="arrow left icon"
-          iconStyle="w-4 h-4 order-2 md:w-5 md:h-5"
-          buttonStyle="bg-transparent mx-auto text-[#315F41] border border-[#315F41] md:hover:text-[#FFF]"
-        />
-      </div>
+    <div className="p-2 col-span-1 row-span-2 text-center md:row-span-1 md:py-4">
+      <h3 className="text-sm font-medium text-[#353535] row-span-1 mb-1 md:mb-3 md:text-lg md:font-semibold">
+        {branch.title}
+      </h3>
+      <p className="text-xs text-[#717171] row-span-1 md:text-sm md:mb-4">
+        {branch.address}
+      </p>
+      <MyButton
+        containerStyle="hidden md:block opacity-0 md:group-hover:opacity-100 transition-all duration-300"
+        label="صفحه شعبه"
+        variant="outline"
+        imageSrc="/assets/images/icons/arrow-left.svg"
+        alt="arrow left icon"
+        iconStyle="w-4 h-4 order-2 md:w-5 md:h-5"
+        buttonStyle="bg-transparent mx-auto text-[#315F41] border border-[#315F41] md:hover:text-[#FFF]"
+      />
     </div>
   );
 }
