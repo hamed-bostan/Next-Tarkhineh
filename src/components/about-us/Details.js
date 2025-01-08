e89b9053723a7d772d7ac49b368abe6b6a6dac96
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-08 21:15:44 +0330

    fix(footer): resolve desktop styles

--- src/components/about-us/Details.js
+++ src/components/about-us/Details.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Details() {
   return (
-    <section className="px-5 py-4">
-      <h3 className="font-bold mb-1 text-[#353535] md:text-lg md:mb-5">درباره ما</h3>
-      <div className="text-xs grid grid-cols-2 gap-x-4 gap-y-2 text-[#717171] md:grid-rows-[auto_1fr] md:text-sm">
+    <section className="px-5 py-4 lg:px-10">
+      <h3 className="text-md font-bold mb-2 text-[#353535]">درباره ما</h3>
+      <div className="text-sm grid grid-cols-2 gap-x-4 gap-y-2 text-[#717171] md:grid-rows-[auto_1fr] lg:gap-x-6">
         <p className="col-span-1 row-span-1 text-justify">
           رستوران‌های زنجیره‌ای ترخینه در سال ۱۳۶۸ افتتاح گردیده‌اند و در طی این
           سال‌ها همواره با ارائه غذاهای باکیفیت و سرویس سریع و به موقع در تلاش
@@ -16,7 +16,7 @@ export default function Details() {
           height={80}
           src="/assets/images/bannerImages/aboutUs_image.png"
           alt="About us image"
-          className="rounded-sm col-span-1 h-full w-full object-cover object-center md:row-span-2 md:rounded-lg"
+          className="rounded-md col-span-1 h-full w-full object-cover object-center md:row-span-2 lg:h-64"
         />
         <p className="col-span-2 text-justify md:col-span-1">
           دراین خصوص ترخینه همیشه در تلاش بوده تا در طی این زمان‌ها کیفیت غذاهای
