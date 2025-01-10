3cebdce5886f02a77f29509d9967b18f4556bc04
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-11 00:26:38 +0330

    feat(restaurant-menu): add desktop styles to foodCategories section

--- src/components/RestaurantMenu/FoodCategoriesList/FoodCategoriesCard.js
+++ src/components/RestaurantMenu/FoodCategoriesList/FoodCategoriesCard.js
@@ -1,11 +1,11 @@
 export default function FoodCategoriesCard({ item }) {
   return (
-    <div className="flex items-center gap-x-1 bg-[#EDEDED] rounded-md px-2 h-6">
-      <span className="text-sm text-[#353535]">{item}</span>
+    <div className="flex items-center gap-x-1 bg-[#EDEDED] rounded-md px-2 h-6 md:rounded-xl md:h-8">
+      <span className="text-xs text-[#353535] md:text-sm">{item}</span>
       <img
         src="/assets/images/icons/arrow-left.svg"
         alt="arrow left icon"
-        className="w-3 h-3"
+        className="w-3 h-3 md:w-4 md:h-4"
       />
     </div>
   );
--- src/components/RestaurantMenu/FoodCategoriesList/index.js
+++ src/components/RestaurantMenu/FoodCategoriesList/index.js
@@ -10,7 +10,7 @@ export default function FoodCategoriesList() {
   ];
 
   return (
-    <div className="flex gap-2 flex-wrap pr-5">
+    <div className="flex gap-2 flex-wrap pr-5 md:gap-3">
       {FoodCategories.map((item, index) => (
         <FoodCategoriesCard item={item} key={index} />
       ))}
