05886b49b78b2c741987bb49dcaf605cd4dd4e04
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-10 12:40:05 +0330

    feat(restaurant-branch): add desktop styles to address section

--- src/components/RestaurantBranch/Address.js
+++ src/components/RestaurantBranch/Address.js
@@ -1,27 +1,31 @@
 export default function Address() {
   return (
-    <div className="grid grid-cols-2 text-sm text-[#353535] gap-y-2 p-2 border-2 border-[#417F56] rounded-sm absolute -bottom-14 left-4 right-4 bg-white">
-      <div className="col-span-full flex gap-x-1 items-center">
+    <div className="grid grid-cols-2 text-xs text-[#353535] gap-y-2 p-2 border-2 border-[#417F56] rounded-sm absolute -bottom-14 left-4 right-4 bg-[#FFFFFF] md:grid-cols-3 lg:text-sm md:left-20 md:right-20 lg:left-56 lg:right-56 lg:p-3">
+      <div className="col-span-full flex gap-x-1 items-center md:col-start-2 md:col-span-1 md:flex md:flex-col">
         <img
           src="/assets/images/icons/location.svg"
           alt="location icon"
-          className="w-4 h-4"
+          className="w-4 h-4 md:mb-2 md:w-6 md:h-6"
         />
-        <span>شهرک اکباتان، فاز ۳، مجتمع تجاری کوروش، طبقه سوم</span>
+        <span className="text-center">
+          شهرک اکباتان، فاز ۳، مجتمع
+          <span className="md:block">تجاری کوروش، طبقه سوم</span>
+        </span>
       </div>
-      <div className="flex gap-x-1 items-center">
+      <div className="flex gap-x-1 items-center md:col-start-1 md:row-start-1 md:flex md:flex-col">
         <img
           src="/assets/images/icons/call-calling.svg"
           alt="phone call icon"
-          className="w-4 h-4"
+          className="w-4 h-4 md:mb-2 md:w-6 md:h-6"
         />
-        <span>۰۲۱-۳۳۵۳۵۳۵۴</span>
+        <span className="">۰۲۱-۳۳۵۳۵۳۵۴</span>
+        <span className="hidden md:block">۰۲۱-۳۳۵۳۵۳۵۶</span>
       </div>
-      <div className="flex gap-x-1 items-center">
+      <div className="flex gap-x-1 md:col-start-3 md:flex md:flex-col md:items-center">
         <img
           src="/assets/images/icons/clock.svg"
           alt="clock icon"
-          className="w-4 h-4"
+          className="w-4 h-4 md:mb-2 md:w-6 md:h-6"
         />
         <span>همه روزه از ساعت ۱۲ الی ۲۳ </span>
       </div>
