870f885b99ab93b7c38b4b43786bbc08dab2db90
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-14 23:17:18 +0330

    fix(restaurant-branch): resolve foodList styles

--- src/components/RestaurantBranch/foodHighlights/FoodList.js
+++ src/components/RestaurantBranch/foodHighlights/FoodList.js
@@ -2,15 +2,22 @@ import { Carousel, CarouselContent } from "@/components/ui/carousel";
 import { foodInformation } from "@/lib/data";
 import FoodCard from "./FoodCard";
 
-export default function FoodList({ title, filter }) {
+export default function FoodList({
+  title,
+  filter,
+  containerStyle = "",
+  titleStyle = "text-[#353535]",
+}) {
   const filteredFood = foodInformation.filter((item) => item.filter === filter);
   const nonIranianFood = foodInformation.filter(
     (item) => item.category === filter
   );
 
   return (
-    <div className="px-5 mb-6">
-      <span className="font-bold text-[#353535] mb-3 block md:text-lg md:mb-5">{title}</span>
+    <div className={`p-5 md:p-6 lg:p-7 ${containerStyle}`}>
+      <span className={`font-bold mb-3 block md:text-lg md:mb-5 ${titleStyle}`}>
+        {title}
+      </span>
       <Carousel rtl>
         <CarouselContent>
           {filteredFood.map((item) => (
--- src/components/RestaurantBranch/foodHighlights/index.js
+++ src/components/RestaurantBranch/foodHighlights/index.js
@@ -4,7 +4,12 @@ export default function FoodHighlights() {
   return (
     <>
       <FoodList title="پیشنهاد ویژه" filter="پیشنهاد ویژه" />
-      <FoodList title="غذاهای محبوب" filter="غذاهای محبوب" />
+      <FoodList
+        title="غذاهای محبوب"
+        filter="غذاهای محبوب"
+        containerStyle="bg-[#315F41]"
+        titleStyle="text-[#fff]"
+      />
       <FoodList title="غذاهای غیر ایرانی" filter="غذاهای غیر ایرانی" />
     </>
   );
