65e7803a4ba724669b1860ddf040d699a25c6ca5
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-16 21:27:47 +0330

    chore: remove unnecessary comments

--- src/components/checkout/index.js
+++ src/components/checkout/index.js
@@ -7,19 +7,15 @@ import Payments from "./payments";
 import ShoppingCart from "./shoppingCart";
 
 export default function Checkout() {
-  // State to track the active tab
-  const [activeTab, setActiveTab] = useState(0); // 0: ShoppingCart, 1: OrderOverview, 2: Payments
+  const [activeTab, setActiveTab] = useState(0);
 
-  // Array of components to render
   const tabs = [ShoppingCart, OrderOverview, Payments];
 
-  // Get the active tab component
   const ActiveTabComponent = tabs[activeTab];
 
   return (
     <section className="px-5 py-6">
       <Header setActiveTab={setActiveTab} activeTab={activeTab} />
-      {/* Render the active tab component */}
       <ActiveTabComponent />
     </section>
   );
