64d8eab78604046fe785b08b5b675305386cdde2
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-16 15:52:56 +0330

    refactor(checkout): restructure cartSummary

--- src/components/checkout/shoppingCart/cartDetails/index.js
+++ src/components/checkout/shoppingCart/cartDetails/index.js
@@ -2,25 +2,21 @@
 
 import { useSelector } from "react-redux";
 
-import { Separator } from "@/components/ui/separator";
-import CartSummary from "./CartSummary";
 import ItemsMobile from "./itemsMobile";
 import ItemsDesktop from "./itemsDesktop";
 import EmptyCartMessage from "./EmptyCartMessage";
+import { Separator } from "@/components/ui/separator";
 
 export default function CartDetails() {
   const selectedItems = useSelector((state) => state.cart.selectedItems);
 
   return (
-    <div className="border border-[#CBCBCB] rounded-lg p-5 md:border-none md:p-0">
+    <div>
       {selectedItems.length > 0 ? (
         <>
           <ItemsMobile selectedItems={selectedItems} />
-          <div className="md:grid md:grid-cols-[60fr_40fr] md:gap-x-4 lg:gap-x-6">
-            <ItemsDesktop selectedItems={selectedItems} />
-            <Separator className="md:hidden" />
-            <CartSummary />
-          </div>
+          <ItemsDesktop selectedItems={selectedItems} />
+          <Separator className="md:hidden" />
         </>
       ) : (
         <EmptyCartMessage />
--- src/components/checkout/shoppingCart/cartDetails/itemsMobile/index.js
+++ src/components/checkout/shoppingCart/cartDetails/itemsMobile/index.js
@@ -1,11 +1,15 @@
+import CartSummary from "../CartSummary";
 import ItemsCard from "./ItemsCard";
 
 export default function ItemsMobile({ selectedItems }) {
   return (
-    <div className="h-52 overflow-y-scroll mb-3 md:hidden">
-      {selectedItems.map((foodItem, index) => (
-        <ItemsCard foodItem={foodItem} key={foodItem.id} index={index} />
-      ))}
+    <div className="border border-[#CBCBCB] rounded-lg p-5 md:hidden">
+      <div className="h-52 overflow-y-scroll mb-3">
+        {selectedItems.map((foodItem, index) => (
+          <ItemsCard foodItem={foodItem} key={foodItem.id} index={index} />
+        ))}
+      </div>
+      <CartSummary />
     </div>
   );
 }
