626651c313889c1363372c1b3958124d72012284
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-18 19:29:27 +0330

    fix(restaurant-menu): resolve checkoutButton issue

--- src/components/RestaurantMenu/CheckoutButton.js
+++ src/components/RestaurantMenu/CheckoutButton.js
@@ -3,15 +3,17 @@ import MyButton from "../common/MyButton";
 
 export default function CheckoutButton() {
   return (
-    <Link href="/checkout">
-      <MyButton
-        variant="outline"
-        label="تکمیل خرید"
-        imageSrc="/assets/images/icons/shopping-cart.svg"
-        buttonStyle="text-[#417F56] border border-[#417F56] hover:text-[#fff] w-24 h-8 md:w-32 lg:w-44 md:h-10 mr-auto"
-        alt="shopping cart icon"
-        iconStyle="w-4 h-4 md:h-5 md:w-5"
-      />
-    </Link>
+    <div className="w-fit mr-auto">
+      <Link href="/checkout">
+        <MyButton
+          variant="outline"
+          label="تکمیل خرید"
+          imageSrc="/assets/images/icons/shopping-cart.svg"
+          buttonStyle="text-[#417F56] border border-[#417F56] hover:text-[#fff] w-24 h-8 md:w-32 lg:w-44 md:h-10"
+          alt="shopping cart icon"
+          iconStyle="w-4 h-4 md:h-5 md:w-5"
+        />
+      </Link>
+    </div>
   );
 }
