61e128dc8110e94e523d5652151a4794620c62ae
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-21 17:27:05 +0330

    feat(checkout): add totalDiscountedPrice to itemsCard mobile version

--- src/components/checkout/shoppingCart/cartDetails/items/itemsMobile/ItemsCard.js
+++ src/components/checkout/shoppingCart/cartDetails/items/itemsMobile/ItemsCard.js
@@ -2,19 +2,32 @@ import QuantitySelector from "@/components/common/QuantitySelector";
 import { useDispatch, useSelector } from "react-redux";
 import { increase, decrease, removeItem } from "@/redux/actions/cartAction";
 import formatToPersianStyle from "@/lib/formattedPrice";
+import discountPrice from "@/lib/discountPrice";
 
 export default function ItemsCard({ foodItem, index }) {
+  const { id, price, discount, title } = foodItem;
+
   const dispatch = useDispatch();
   const selectedItems = useSelector((state) => state.cart.selectedItems);
 
   // Check if the current food item is selected
-  const selectedItem = selectedItems.find((item) => item.id === foodItem.id);
+  const selectedItem = selectedItems.find((item) => item.id === id);
 
-  const handleIncrease = () => dispatch(increase(foodItem.id));
+  const handleIncrease = () => dispatch(increase(id));
+  const handleDecrease = () => dispatch(decrease(id));
+  const handleRemove = () => dispatch(removeItem(id));
 
-  const handleDecrease = () => dispatch(decrease(foodItem.id));
+  const discountedPricePerItem = discountPrice(price, discount); // Price of a single item
 
-  const handleRemove = () => dispatch(removeItem(foodItem.id));
+  // Check if the current food item is selected
+  const quantity = selectedItem?.quantity || 0;
+
+  // Calculate total discounted price for the selected item
+  const totalDiscountedPrice =
+    quantity > 0
+      ? formatToPersianStyle(discountedPricePerItem * quantity)
+      : formatToPersianStyle(discountedPricePerItem);
+
   return (
     <div
       className={`grid grid-cols-2 p-2 ${
@@ -22,10 +35,8 @@ export default function ItemsCard({ foodItem, index }) {
       }`}
     >
       <div>
-        <h3 className="text-[#353535] text-sm">{foodItem.title}</h3>
-        <span className="text-[#717171] text-xs">
-          {formatToPersianStyle(foodItem.price)}
-        </span>
+        <h3 className="text-[#353535] text-sm">{title}</h3>
+        <span className="text-[#717171] text-xs">{totalDiscountedPrice}</span>
       </div>
       <QuantitySelector
         selectedItem={selectedItem}
