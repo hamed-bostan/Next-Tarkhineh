157ffbf4d17c51f5e729254b26e8df262d17a591
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-01-30 00:29:06 +0330

    chore(api/options): remove google provider

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,4 @@
 import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 
 export const options = {
   providers: [
@@ -19,20 +18,6 @@ export const options = {
       },
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_Secret,
-    }),
-    GoogleProvider({
-      profile(profile) {
-        console.log("Profile Google: ", profile);
-
-        let userRole = "Google User";
-        return {
-          ...profile,
-          id: profile.sub,
-          role: userRole,
-        };
-      },
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_Secret,
     }),
   ],
   callbacks: {
