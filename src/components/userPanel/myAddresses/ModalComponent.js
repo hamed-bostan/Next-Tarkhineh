7e2be6e3df3fbfebd51e0fcd469625fe699ea85c
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-02 15:08:10 +0330

    feat(myAddresses): add modal to display the map

--- src/components/userPanel/myAddresses/ModalComponent.js
+++ src/components/userPanel/myAddresses/ModalComponent.js
@@ -1,14 +1,26 @@
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
 import FetchAddress from "./FetchAddress";
 
-export function ModalComponent() {
+export function ModalComponent({ isOpen, onClose }) {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px] p-0">
+        <VisuallyHidden>
+          <DialogHeader>
+            <DialogTitle>افزودن آدرس جدید</DialogTitle>
+            <DialogDescription>برای اضافه کردن آدرس</DialogDescription>
+          </DialogHeader>
+        </VisuallyHidden>
         <FetchAddress />
       </DialogContent>
     </Dialog>
--- src/components/userPanel/myAddresses/index.js
+++ src/components/userPanel/myAddresses/index.js
@@ -1,30 +1,31 @@
+"use client";
+
 import EmptyStateMessage from "@/components/common/EmptyStateMessage";
 import FetchAddress from "./FetchAddress";
 import Details from "./Details";
 import { useSelector } from "react-redux";
+import { ModalComponent } from "./ModalComponent";
+import { useState } from "react";
 
 export default function MyAddresses() {
   const address = useSelector((state) => state.address.address); // Get address from Redux
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <EmptyStateMessage
-      text="شما در حال حاضر هیچ آدرسی ثبت نکرده‌اید!"
-      button={true}
-      buttonText="افزودن آدرس"
-    />
+    <>
+      {!address && (
+        <EmptyStateMessage
+          text="شما در حال حاضر هیچ آدرسی ثبت نکرده‌اید!"
+          button={true}
+          buttonText="افزودن آدرس"
+          onClick={() => setIsModalOpen(true)} // Open modal on click
+        />
+      )}
+
+      <ModalComponent
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 }
-
-// return (
-//   <>
-//     {address ? (
-//       <Details />
-//     ) : (
-//       <EmptyStateMessage
-//         text="شما در حال حاضر هیچ آدرسی ثبت نکرده‌اید!"
-//         button={true}
-//         buttonText="افزودن آدرس"
-//       />
-//     )}
-//   </>
-// );
