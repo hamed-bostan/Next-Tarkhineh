61e7433d28591160a616d960bd756870961210dd
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-02 13:20:56 +0330

    feat(myAddresses): add noAddress component

--- src/components/userPanel/index.js
+++ src/components/userPanel/index.js
@@ -3,11 +3,11 @@
 import { useState } from "react";
 import OrderStatus from "./orderStatus";
 import Favorites from "./favorites";
-import MyAddresses from "./myAddresses";
 import Logout from "./logout";
 import Sidebar from "./Sidebar";
 import Profile from "./profile";
 import HeaderMobile from "./header/HeaderMobile";
+import MyAddresses from "./myAddresses";
 
 export default function UserPanel() {
   const [activeTab, setActiveTab] = useState(0);
@@ -20,7 +20,7 @@ export default function UserPanel() {
     <section className="px-5 py-6">
       <HeaderMobile setActiveTab={setActiveTab} activeTab={activeTab} />
       <div className="md:grid md:grid-cols-[30fr_70fr] md:gap-x-5">
-        <Sidebar setActiveTab={setActiveTab} activeTab={activeTab}/>
+        <Sidebar setActiveTab={setActiveTab} activeTab={activeTab} />
         <ActiveTabComponent />
       </div>
     </section>
--- src/components/userPanel/myAddresses/index.js
+++ src/components/userPanel/myAddresses/index.js
@@ -1,39 +1,14 @@
-"use client";
-import { useState } from "react";
-import LocationPicker from "./LocationPicker";
+import EmptyCartMessage from "@/components/checkout/EmptyCartMessage";
+import FetchAddress from "./FetchAddress";
 
 export default function MyAddresses() {
-  const [location, setLocation] = useState(null);
-  const [address, setAddress] = useState(null);
-
-  // Function to get address from coordinates
-  const fetchAddress = async (lat, lon) => {
-    try {
-      const response = await fetch(
-        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
-      );
-      const data = await response.json();
-      return data.display_name; // Full address
-    } catch (error) {
-      console.error("Error fetching address:", error);
-      return "Address not found";
-    }
-  };
-
-  // Function to handle location selection
-  const handleLocationSelect = async (lat, lon) => {
-    setLocation({ lat, lon });
-
-    const fetchedAddress = await fetchAddress(lat, lon);
-    setAddress(fetchedAddress);
-  };
-
   return (
-    <div className="">
-      <LocationPicker
-        onLocationSelect={handleLocationSelect}
-        address={address}
+    <>
+      <EmptyCartMessage
+        text="شما در حال حاضر هیچ آدرسی ثبت نکرده‌اید!"
+        button="افزودن آدرس"
       />
-    </div>
+      <FetchAddress />
+    </>
   );
 }
