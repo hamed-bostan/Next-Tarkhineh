9b1979f0fe7835a4e9a7b8e5d2a9af138db66f3d
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-05 16:58:26 +0330

    refactore(userPanel/myAddresses): remove myAddressContent for better structure

--- src/components/userPanel/myAddresses/index.js
+++ src/components/userPanel/myAddresses/index.js
@@ -1,9 +1,29 @@
-import MyAddressesContent from "./MyAddressesContent";
+"use client";
+
+import { useSelector } from "react-redux";
+import EmptyStateMessage from "@/components/common/EmptyStateMessage";
+import { GeolocationDialog } from "./geolocationDialog";
+import AddressDialog from "./addressDialog";
+import { AddressProvider } from "@/context/AddressContext";
+import UserAddresses from "./userAddresses";
 
 export default function MyAddresses() {
+  const addresses = useSelector((state) => state.address.addresses); // Get address from Redux
+
   return (
-    <>
-      <MyAddressesContent />
-    </>
+    <AddressProvider>
+      {addresses.length === 0 ? (
+        <EmptyStateMessage
+          text="شما در حال حاضر هیچ آدرسی ثبت نکرده‌اید!"
+          button={true}
+          buttonText="افزودن آدرس"
+        />
+      ) : (
+        <UserAddresses />
+      )}
+
+      <GeolocationDialog />
+      <AddressDialog />
+    </AddressProvider>
   );
 }
