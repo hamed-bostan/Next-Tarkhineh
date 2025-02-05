64e94ad27c939e3e317a3c1057d8798c25fececb
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-05 16:47:43 +0330

    fix(AddressDialogProvider): fix wrapper problem

--- src/components/redux/ProviderWrapper.js
+++ src/components/redux/ProviderWrapper.js
@@ -3,12 +3,13 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "@/redux/store";
+import { AddressDialogProvider } from "@/context/AddressDialogContext";
 
 export default function ProviderWrapper({ children }) {
   return (
     <Provider store={store}>
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-        {children}
+        <AddressDialogProvider>{children}</AddressDialogProvider>
       </PersistGate>
     </Provider>
   );
--- src/components/userPanel/myAddresses/index.js
+++ src/components/userPanel/myAddresses/index.js
@@ -1,10 +1,9 @@
-import { AddressDialogProvider } from "@/context/AddressDialogContext";
 import MyAddressesContent from "./MyAddressesContent";
 
 export default function MyAddresses() {
   return (
-    <AddressDialogProvider>
+    <>
       <MyAddressesContent />
-    </AddressDialogProvider>
+    </>
   );
 }
