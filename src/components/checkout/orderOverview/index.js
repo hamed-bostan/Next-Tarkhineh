9b78e48ff2adea46c2941ba5131ace6147f8a560
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-05 10:32:16 +0330

    feat(checkout): add different style for different routes on userAddressList && add dynamic cartSummary

--- src/components/checkout/orderOverview/index.js
+++ src/components/checkout/orderOverview/index.js
@@ -1,18 +1,19 @@
 import MyAddresses from "@/components/userPanel/myAddresses";
 import Address from "./address";
 import MoreInformation from "./address/MoreInformation";
-import CartSummaryOverview from "./CartSummaryOverview";
 import OrderDeliveryMethod from "./OrderDeliveryMethod";
+import CartSummary from "../CartSummary";
 
 export default function OrderOverview() {
   return (
     <section className="md:grid md:grid-cols-[60fr_40fr] md:gap-x-4 lg:gap-x-6">
       <div>
         <OrderDeliveryMethod />
+        {/* <Address /> */}
         <MyAddresses />
         <MoreInformation />
       </div>
-      <CartSummaryOverview />
+      <CartSummary />
     </section>
   );
 }
--- src/components/userPanel/myAddresses/userAddresses/index.js
+++ src/components/userPanel/myAddresses/userAddresses/index.js
@@ -3,12 +3,18 @@ import MyButton from "@/components/common/MyButton";
 import HeaderDesktop from "../../header/HeaderDesktop";
 import { useAddressDialog } from "@/context/AddressDialogContext";
 import UserAddressList from "./UserAddressList";
+import { usePathname } from "next/navigation";
 
 export default function UserAddresses() {
   const { openGeolocationDialog } = useAddressDialog(); // Access the context values
+  const pathname = usePathname();
 
   return (
-    <div className="md:border md:border-[#CBCBCB] md:rounded-lg md:p-5">
+    <div
+      className={`md:border md:border-[#CBCBCB] md:rounded-lg md:p-5 ${
+        pathname === "/checkout" ? "border border-[#CBCBCB] p-4 pb-6 rounded-lg" : ""
+      }`}
+    >
       <HeaderDesktop
         label="آدرس ها"
         style="mb-4"
