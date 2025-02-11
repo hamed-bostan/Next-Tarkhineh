5ef245560b45344d6b8b10702b75189f6f7a28f7
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-11 11:54:15 +0330

    refactore(franchise/requestForm): remove image map

--- src/components/franchise/requestForm/AddressProperty.js
+++ src/components/franchise/requestForm/AddressProperty.js
@@ -1,23 +1,15 @@
 import MyInput from "@/components/common/MyInput";
 import { Textarea } from "@/components/ui/textarea";
-import Image from "next/image";
 
 export default function AddressProperty() {
   return (
     <div className="mb-12">
       <span className="mb-6 block">آدرس ملک متقاضی</span>
-      <div className="grid grid-cols-3 gap-x-4 gap-y-4 grid-rows-3">
-        <MyInput placeholder="استان" style="h-full" />
-        <MyInput placeholder="شهر" style="h-full" />
-        <Image
-          src="/assets/images/franchise/map.jpg"
-          width={150}
-          height={100}
-          alt="map image"
-          className="row-span-3 w-full h-full rounded-sm border border-[#CBCBCB]"
-        />
+      <div className="grid grid-cols-2 gap-4">
+        <MyInput placeholder="استان" />
+        <MyInput placeholder="شهر" />
         <MyInput placeholder="منطقه" style="h-full" />
-        <Textarea placeholder="آدرس دقیق" className="row-span-2 resize-none" />
+        <Textarea placeholder="آدرس دقیق" className="resize-none" />
       </div>
     </div>
   );
