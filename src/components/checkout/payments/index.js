4b0f27b53213c3fa02f83a2258bbd998b4b62565
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-15 09:31:44 +0330

    feat(userPanel): add paymentGateway component

--- src/components/checkout/payments/index.js
+++ src/components/checkout/payments/index.js
@@ -1,11 +1,13 @@
 import DiscountCode from "./DiscountCode";
+import PaymentGateway from "./PaymentGateway";
 import PaymentMethod from "./PaymentMethod";
 
 export default function Payments() {
   return (
     <>
       <DiscountCode />
-      <PaymentMethod/>
+      <PaymentMethod />
+      <PaymentGateway />
     </>
   );
 }
