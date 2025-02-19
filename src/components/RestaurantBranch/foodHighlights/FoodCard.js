85738b06f56a8d06e072f3cf3b64f1cde780d8a0
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-02-19 11:59:13 +0330

    fix(restaurantBranch): improve styles

--- src/components/RestaurantBranch/foodHighlights/FoodCard.js
+++ src/components/RestaurantBranch/foodHighlights/FoodCard.js
@@ -13,7 +13,7 @@ import { addItem } from "@/redux/actions/cartAction";
 export default function FoodCard({ item }) {
   return (
     <CarouselItem className="basis-auto">
-      <Card className="shadow-none border border-[#CBCBCB] rounded-sm overflow-hidden w-48 md:min-h-80 md:w-52 md:rounded-lg">
+      <Card className="shadow-none border border-[#CBCBCB] rounded-sm overflow-hidden w-48 md:w-52 md:rounded-lg">
         <CardContent className="p-0">
           <DisplayingImage item={item} />
           <OfferDetails item={item} />
@@ -53,17 +53,17 @@ function OfferDetails({ item }) {
   }
 
   return (
-    <div className="col-span-2 row-span-3 grid grid-cols-2 p-2 pt-1 text-[#353535] text-xs gap-y-1 md:text-sm md:gap-y-2 md:p-3">
+    <div className="col-span-2 row-span-3 grid grid-cols-2 p-2 pt-1 text-[#353535] text-xs gap-y-1 md:text-sm md:gap-y-2 md:p-3 h-32 md:h-40">
       <h3 className="col-span-full text-center text-sm md:text-base md:font-semibold">
         {title}
       </h3>
       <Heart color="#ADADAD" className="w-4 h-4 cursor-pointer" />
       {discount && (
-        <div className="flex gap-x-1 items-center mr-auto">
+        <div className="flex gap-x-1 items-center mr-auto w-full">
           <span className="text-[#ADADAD] line-through">
             {formatToPersianStyle(price)}
           </span>
-          <span className="text-[#C30000] bg-[#FFF2F2] rounded-lg w-8 text-center">
+          <span className="text-[#C30000] bg-[#FFF2F2] rounded-lg text-center mr-auto min-w-8 md:min-w-10">
             {formatToPersianStyle(discount)} %
           </span>
         </div>
@@ -82,7 +82,7 @@ function OfferDetails({ item }) {
       <MyButton
         onClick={handleAddToCart}
         label={isAddedToCart ? "افزوده شد" : "افزودن به سبد خرید"}
-        buttonStyle={`col-span-2 mt-3 bg-[#417F56] md:text-sm md:mt-4 ${
+        buttonStyle={`col-span-2 bg-[#417F56] md:text-sm flex self-end ${
           isAddedToCart ? "bg-[#717171] pointer-events-none" : "bg-[#417F56] "
         }`}
       />
