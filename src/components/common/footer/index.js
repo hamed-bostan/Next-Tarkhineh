1c255e44406808303a689a824b045e9f081ca1c2
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-03-03 17:46:14 +0330

    fix(footer): improve styles

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 export default function Footer() {
   return (
-    <section className="bg-[url('/assets/images/bannerImages/banner-03.jpg')] bg-center bg-cover w-full grid grid-cols-2 px-5 py-4">
+    <section className="bg-[url('/assets/images/bannerImages/banner-03.jpg')] bg-center bg-cover w-full grid grid-cols-2 px-5 py-4 lg:px-10 2xl:px-28">
       <Navigation />
       <ContactForm />
     </section>
